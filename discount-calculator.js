3b70f9f864e564868073c3acf2b197c2e2d9f4d5
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-16 14:49:52 +0100

    Added Discount-calculator.js

---
+++ discount-calculator.js
@@ -0,0 +1,17 @@
+/*
+Exercise 12: Discount Calculator
+Declare  price = 8000  and  discountPercent = 15 .
+Calculate and print:
+The discount amount
+The final price after discount
+Expected output:
+*/
+
+let price = 8000;
+let discountPercent = 15;
+
+let discountAmount = price * 0.85;
+let finalPrice = price * 0.15;
+
+console.log(`Final Price: ₦${finalPrice}`);
+console.log(`Discount Amount: ₦${discountAmount}`);
