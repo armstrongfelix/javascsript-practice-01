dd35a7598d717a090dfa6fc6b64dce34e5b17bcc
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-16 10:28:02 +0100

    added rectangle.js file

---
+++ rectangle.js
@@ -0,0 +1,8 @@
+const length = 12;
+const width = 7;
+
+let area = length * width;
+let perimeter = 2 * (length + width);
+
+console.log(`The area of the rectangle is: ${area}`);
+console.log(`The perimeter of the rectangle is: ${perimeter}`);
