85c13495e1aff7983ccefcbfdaf813bba2501245
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-16 09:54:07 +0100

    task initiated, added thee favourite.js file

---
+++ favourite.js
@@ -0,0 +1,8 @@
+const favouriteColor = "Blue";
+const favouriteFood = "Moimoi";
+const favouriteSports = "Volley Ball";
+const favouriteMovie = "The Legend";
+
+console.log(
+  `My Favourite Colour is ${favouriteColor}, I love eating ${favouriteFood}, and I enjoy playing ${favouriteSports}, and my favourite movie is ${favouriteMovie}`,
+);
